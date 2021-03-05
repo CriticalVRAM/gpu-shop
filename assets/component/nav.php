a7a54f0ce8ae12a311c7a0b438f5c2589fa7a3e6
Nani-Sore-0 <urke.radule@gmail.com>
2021-03-05 17:38:40 +0100

    nav finished

---
+++ assets/component/nav.php
@@ -0,0 +1,45 @@
+<div class="navbar-expand">
+    <nav class="navbar">
+        <a href="index.php"><img class="navbar-brand" src="assets/img/logo_small.png" alt="logo"></a>
+
+        <ul class="navbar-nav mr-auto">
+
+            <?php
+
+            $is_admin = 0;
+            $is_logedin = isset($_SESSION["userID"]);
+            if ($is_logedin) $is_admin = $_SESSION["userID"] == 6;
+
+            if ($is_admin) : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin-pannel.php">Admin Panel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+            <?php elseif ($is_logedin) : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="cart.php">Cart</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="survey.php">Survey</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+            <?php else : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php">Register</a>
+                </li>
+            <?php endif; ?>
+
+        </ul>
+
+    </nav>
+</div>
--- index.php
+++ index.php
@@ -1,39 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php require_once "assets/component/header.php" ?>
+<?php require "assets/component/header.php" ?>
 <?php session_start(); ?>
 
 <body>
-  <div class="navbar-expand">
-    <nav class="navbar">
-      <img class="navbar-brand" src="assets/img/logo_small.png" alt="logo">
-
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Shop</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="login.php">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Register</a>
-        </li>
-        <?php
-
-        if (isset($_SESSION["userID"])) : ?>
-          <li class="nav-item">
-            <a class="nav-link" href="logout.php">Logout</a>
-          </li>
-        <?php endif; ?>
-
-
-      </ul>
-
-    </nav>
-  </div>
-
-
-
+  <?php require "assets/component/nav.php" ?>
+  <h2>This is the body</h2>
 </body>
 
 </html>
